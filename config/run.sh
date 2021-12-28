6088a105b3404a58ed23bd346fdb865f813823aa
Rob Matesick <rob.matesick@gmail.com>
2021-12-28 10:11:04 -0500

    Clarified use of cronjob_iteration

--- config/run.sh
+++ config/run.sh
@@ -6,7 +6,8 @@ TMP_FILE=/tmp/crontab.$$
 cat /var/www/html/config/crontab.default > ${TMP_FILE}
 
 if [[ ${CRONJOB_ITERATION} && ${CRONJOB_ITERATION-x} ]]; then
-    sed -i -e "s/0/\*\/${CRONJOB_ITERATION}/g" ${TMP_FILE}
+    #sed -i -e "s/0/\*\/${CRONJOB_ITERATION}/g" ${TMP_FILE}
+    sed -i -e "s/0/${CRONJOB_ITERATION}/g" ${TMP_FILE}
 fi
 crontab ${TMP_FILE}
 
